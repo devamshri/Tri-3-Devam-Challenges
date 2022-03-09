9aa9c54acc654ea78eef5af9058d902aabcdae9e
devamshri <devamshri@gmail.com>
2022-03-09 10:36:27 -0800

    Challenge 2 Complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,12 @@
-package com.company.IntByReference;
+//package com.company.IntByReference;
 
 public class Main {
 
     // static main method that provides some simple test cases
     public static void main(String[] ags) {
-        IntByReference.swapper(21, 16);
-        IntByReference.swapper(16, 21);
-        IntByReference.swapper(16, -1);
+//        IntByReference.swapper(21, 16);
+//        IntByReference.swapper(16, 21);
+//        IntByReference.swapper(16, -1);
 
 
     }
