315631cd65a078ac40c4847eb15fed2dae370e95
devamshri <devamshri@gmail.com>
2022-04-06 02:27:43 -0700

    complete all three sorts in linked list

--- src/com/devamchallenges/tt3/sorttesting/LinkedList.java
+++ src/com/devamchallenges/tt3/sorttesting/LinkedList.java
@@ -1,6 +1,15 @@
 package com.devamchallenges.tt3.sorttesting;
 
 
+import com.devamchallenges.tt3.BubbleSort;
+import com.devamchallenges.tt3.InsertionSort;
+import com.devamchallenges.tt3.MergeSort;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class LinkedList<T> {
     private T data;
     private com.devamchallenges.tt3.sorttesting.LinkedList<T> prevNode, nextNode;
@@ -95,24 +104,31 @@ public class LinkedList<T> {
             while (j != null && (int) j.getData() > key) {
                 temp = j;
                 j = j.getPrevious();
-
-
-
-
             }
             if (j != null) {
                 j.setNextNode(arr);
-
             }
             else if (temp != null) {
                 temp.setPrevNode(arr);
             }
-
-
             arr = next;
         }
-
     }
+
+    public int len()
+    {
+        LinkedList a =this;
+        while (a.getPrevious() != null) {
+            a = a.getPrevious();
+        }
+         int i = 0;
+        while (a != null){
+            i+= 1;
+            a = a.getNext();
+        }
+        return i;
+    }
+
     public static void printArray(LinkedList arr)
     {
         LinkedList a = arr;
@@ -126,12 +142,65 @@ public class LinkedList<T> {
         System.out.println();
     }
 
-    public static void getFirst(LinkedList arr) {
+    public static void bubbleSort(LinkedList arr) {
+        int i = arr.len();
+        int b = 0;
+        while (b < i) {
+            LinkedList next = arr.getNext();
+            LinkedList j = arr;
+            while (j.getPrevious() != null) {
+                j = j.getPrevious();
+            }
 
+            LinkedList j_1 = j.getNext(); //j+1
+            while (j_1 != null ) {
+                if ((int) j.getData() > (int) j_1.getData()) {
+                    j.remove();
+                    j_1.setNextNode(j);
+                    j_1 = j.getNext();
+                } else {
+                    j_1 = j_1.getNext();
+                }
+
+            }
+
+            arr = next;
+            b+= 1;
+        }
+    }
+    public static void selectionsort(LinkedList arr){
+
+        while (arr != null){
+            // get the minimum
+            LinkedList min = minvalue(arr);
+            if (min == arr) {
+                arr = arr.getNext();
+            }
+
+            if (arr != null) {
+                min.remove();
+                arr.setPrevNode(min);
+            }
+
+        }
     }
 
-    public static void main(String[] args) {
-//        Object[] words = new String[]{"seven", "slimy", "snakes", "sallying", "slowly", "slithered", "southward"};
+    // make a function that finds the minimum from that point onwards
+    public static LinkedList minvalue(LinkedList arr) {
+        int min_key = 99999;
+        LinkedList min = null;
+        while (arr != null){
+            int key = (int) arr.getData();
+            LinkedList next = arr.getNext();
+            if (key < min_key) {
+                min_key = key;
+                min = arr;
+            }
+            arr = next;
+        }
+        return min;
+    }
+    public static void sortlinkedlistmenu() {
         int[] numbers = new int[]{64,25,12,22,11};
 
         LinkedList qWords = new LinkedList(numbers[0]);
@@ -139,10 +208,53 @@ public class LinkedList<T> {
         for (int i = 1; i < numbers.length; i++) {
             qWords = new LinkedList(numbers[i], qWords);
         }
-//        LinkedList.printArray(start);
-//        LinkedList start2 = start;
-        LinkedList.insertsort(start);
-        //LinkedList.getFirst(start);
-        LinkedList.printArray(start);
+
+        // Make sure to include an if-statement to choose which sorting algorithm is to be used
+        String[] menu_items = new String[] {"Welcome to my sort algorithm!\nOption 1: Insertion", "Option 2: Bubble", "Option 3: Selection"};
+
+        for(int i = 0; i < menu_items.length; i++){
+            System.out.println(menu_items[i]);
+            System.out.println("-------------------------\n");
+        }
+        System.out.println("Enter your choice:");
+
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+        try {
+            System.out.println(menu_items[Integer.parseInt(input)-1]);
+        } catch (NumberFormatException e) {
+            System.out.println("Make sure to enter a number as your choice!");
+        } catch (ArrayIndexOutOfBoundsException a){
+            System.out.println("Please enter a choice between 1 and " + menu_items.length);
+        }
+
+        if(input.equals("1")){
+            System.out.println("List before sorting");
+            LinkedList.printArray(start);
+            System.out.println("Let the insertion sort commence");
+            LinkedList.insertsort(start);
+            LinkedList.printArray(start);
+
+        }
+        else if(input.equals("2")){
+            System.out.println("List before sorting");
+            LinkedList.printArray(start);
+            System.out.println("Let the bubble sort commence");
+            LinkedList.bubbleSort(start);
+            LinkedList.printArray(start);
+        }
+        else if(input.equals("3")) {
+            System.out.println("List before sorting");
+            LinkedList.printArray(start);
+            System.out.println("Let the selection sort commence");
+            LinkedList.selectionsort(start);
+            LinkedList.printArray(start);
+        }
+        sortlinkedlistmenu();
+    }
+    public static void main(String[] args) {
+//        Object[] words = new String[]{"seven", "slimy", "snakes", "sallying", "slowly", "slithered", "southward"};
+        LinkedList.sortlinkedlistmenu();
     }
 }
